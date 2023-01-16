76c2e1e24875282c80f4ac54061bf8ea7b580b14
Amer <amer2014np@gmail.com>
2023-01-16 23:29:47 +0100

    c

--- C/zad.c
+++ C/zad.c
@@ -2767,76 +2767,149 @@ int main()
 
 
 
-#include <stdio.h>
-typedef struct temperature
-{
- int datum;
- int m7;
- int m13;
- int m19;
-} TEMPERATURE;
-main()
-{
- TEMPERATURE temp[10];
- int i, j, uk7=0, uk13=0, uk19=0, pomd;
- float pr7, pr13, pr19, prdnevna[10], pom;
- FILE *ulaz, *izlaz;
- char ime1[20], ime2[20];
- printf("\n Ime datoteke sa podacima: ");
- scanf("%s",&ime1);
- printf(" Ime datoteke za kreiranje: ");
- scanf("%s",&ime2);
- if((ulaz=fopen(ime1, "r"))== NULL)
- {
- printf("Greska prilikom otvaranja datoteke!\n");
- return 1;
- }
- if((izlaz=fopen(ime2, "w"))== NULL)
- {
- printf("Greska prilikom otvaranja datoteke!\n");
- return 1;
- }
- /*Citanje podataka iz datoteke*/
- /*Racunanje ukupnih temperatura u 7h, 13h i 19h i dnevne prosecne temperature*/
- for(i=0; i<10; i++)
- {
- fscanf(ulaz, "%d%d%d%d", &temp[i].datum, &temp[i].m7,
- &temp[i].m13, &temp[i].m19);
- uk7+=temp[i].m7;
- uk13+=temp[i].m13;
- uk19+=temp[i].m19;
- prdnevna[i]=(float)(temp[i].m7 + temp[i].m13 + temp[i].m19)/3;
- }
- /*Racunjane prosecnih temperatura u 7h, 13h i 19h*/
- pr7=(float)uk7/10;
- pr13=(float)uk13/10;
- pr19=(float)uk19/10;
- /*Sortiranje prosecnih temperatura od najvise ka najnize*/
- for(i=0; i<9; i++)
- for(j=i+1; j<10; j++)
- if(prdnevna[i] < prdnevna[j])
- {
- pom=prdnevna[i];
- pomd=temp[i].datum;
- prdnevna[i]=prdnevna[j];
- temp[i].datum=temp[j].datum;
- prdnevna[j]=pom;
- temp[j].datum=pomd;
- }
- /*Upis podataka u novokreiranu datoteku*/
- for(i=0; i<10; i++)
- fprintf(izlaz, " %02d %.2f\n", temp[i].datum, prdnevna[i]);
- fclose(ulaz);
- fclose(izlaz);
- printf("\n Datoteka je kreirana!\n\n");
- /*Ispis podataka na ekran*/
- printf(" Najtopliji dan %02d.avgust sa temperaturom od %.2f stepeni.\n",
- temp[0].datum, prdnevna[0]);
- printf(" Najhladniji dan %02d.avgust sa temperaturom od %.2f stepeni.\n",
- temp[9].datum, prdnevna[9]);
- printf(" Prosecna temperatura u 07h: %.2f\n", pr7);
- printf(" Prosecna temperatura u 13h: %.2f\n", pr13);
- printf(" Prosecna temperatura u 19h: %.2f\n", pr19);
+// #include <stdio.h>
+// typedef struct temperature
+// {
+//  int datum;
+//  int m7;
+//  int m13;
+//  int m19;
+// } TEMPERATURE;
+// main()
+// {
+//  TEMPERATURE temp[10];
+//  int i, j, uk7=0, uk13=0, uk19=0, pomd;
+//  float pr7, pr13, pr19, prdnevna[10], pom;
+//  FILE *ulaz, *izlaz;
+//  char ime1[20], ime2[20];
+//  printf("\n Ime datoteke sa podacima: ");
+//  scanf("%s",&ime1);
+//  printf(" Ime datoteke za kreiranje: ");
+//  scanf("%s",&ime2);
+//  if((ulaz=fopen(ime1, "r"))== NULL)
+//  {
+//  printf("Greska prilikom otvaranja datoteke!\n");
+//  return 1;
+//  }
+//  if((izlaz=fopen(ime2, "w"))== NULL)
+//  {
+//  printf("Greska prilikom otvaranja datoteke!\n");
+//  return 1;
+//  }
+//  /*Citanje podataka iz datoteke*/
+//  /*Racunanje ukupnih temperatura u 7h, 13h i 19h i dnevne prosecne temperature*/
+//  for(i=0; i<10; i++)
+//  {
+//  fscanf(ulaz, "%d%d%d%d", &temp[i].datum, &temp[i].m7,
+//  &temp[i].m13, &temp[i].m19);
+//  uk7+=temp[i].m7;
+//  uk13+=temp[i].m13;
+//  uk19+=temp[i].m19;
+//  prdnevna[i]=(float)(temp[i].m7 + temp[i].m13 + temp[i].m19)/3;
+//  }
+//  /*Racunjane prosecnih temperatura u 7h, 13h i 19h*/
+//  pr7=(float)uk7/10;
+//  pr13=(float)uk13/10;
+//  pr19=(float)uk19/10;
+//  /*Sortiranje prosecnih temperatura od najvise ka najnize*/
+//  for(i=0; i<9; i++)
+//  for(j=i+1; j<10; j++)
+//  if(prdnevna[i] < prdnevna[j])
+//  {
+//  pom=prdnevna[i];
+//  pomd=temp[i].datum;
+//  prdnevna[i]=prdnevna[j];
+//  temp[i].datum=temp[j].datum;
+//  prdnevna[j]=pom;
+//  temp[j].datum=pomd;
+//  }
+//  /*Upis podataka u novokreiranu datoteku*/
+//  for(i=0; i<10; i++)
+//  fprintf(izlaz, " %02d %.2f\n", temp[i].datum, prdnevna[i]);
+//  fclose(ulaz);
+//  fclose(izlaz);
+//  printf("\n Datoteka je kreirana!\n\n");
+//  /*Ispis podataka na ekran*/
+//  printf(" Najtopliji dan %02d.avgust sa temperaturom od %.2f stepeni.\n",
+//  temp[0].datum, prdnevna[0]);
+//  printf(" Najhladniji dan %02d.avgust sa temperaturom od %.2f stepeni.\n",
+//  temp[9].datum, prdnevna[9]);
+//  printf(" Prosecna temperatura u 07h: %.2f\n", pr7);
+//  printf(" Prosecna temperatura u 13h: %.2f\n", pr13);
+//  printf(" Prosecna temperatura u 19h: %.2f\n", pr19);
 
- return 0;
-}
+//  return 0;
+// }
+
+
+
+int i,niz[100],n,budzet,pom,j,suma=0;
+// printf("unesi n i budzet");
+// scanf("%d  %d",&n,&budzet);
+// for(i=0;i<n;i++){
+//     scanf("%d",&niz[i]);
+// }
+// for(i=0;i<n-1;i++){
+//  for(j=i+1;j<n;j++){
+//      if(niz[i]>niz[j]){
+//          pom=niz[i];
+//          niz[i]=niz[j];
+//          niz[j]=pom;
+//      }
+//  }
+// }
+
+
+//     for(i=0;i<n;i++){
+//         suma+=niz[i];
+//         if(suma>budzet)break;
+//     }
+//     printf("%d",i);
+
+
+// int i,niz[100],n,budzet,pom,j,suma=0,makica[100][100],x,br=0;
+// printf("unesi n");
+// scanf("%d",&n);
+// for(i=0;i<n;i++)
+//     scanf("%d",&niz[i]);
+    
+    
+
+// for(i=0;i<n;i++){
+//  for(j=i+1;j<n;j++){
+//      if(niz[i]>niz[j]){
+//          pom=niz[i];
+//          niz[i]=niz[j];
+//          niz[j]=pom;
+//      }
+//  }
+// }
+
+// for(i=0;i<n;i++){
+//     printf("%d ",niz[i]);
+// }
+// x=niz[1]-niz[0];
+// for(i=0;i<n;i++){
+//     if(niz[i+1]-niz[i]==x)br++;
+// }
+// if(br+1==n)printf("jes");
+// else printf("nije");
+// return 0;
+
+
+//     int main()
+// { int i,mat[100][100],n,budzet,pom,j,suma=0,makica[100][100];
+// printf("unesi n");
+// scanf("%d",&n);
+// for(i=0;i<n;i++)
+// for(j=0;j<n;j++){
+//     scanf("%d",&mat[i][j]);
+// }
+// for(i=0;i<n;i++){
+//  for(j=0;j<n;j++){
+//    if(mat[i][j]%2==0&&i+j==3){
+//        makica[i][j]=mat[i][j];}
+//        else makica[i][j]=0;
+   
+//  }
+// }
